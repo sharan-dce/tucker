8bec873ef2cff06994d8e8382c1cec0403dcdf3b
yangyf <yangyf@rc.inesa.com>
2021-03-27 21:25:20 +0000

    show the calling method of distmult and add batch support for rescal

--- rescal.py
+++ rescal.py
@@ -3,6 +3,7 @@ import numpy as np
 from tucker import get_gradient_masked_tensor_clone
 
 
+# RESCAL model supporting batches of subjects and relations
 class RESCAL(torch.nn.Module):
     def __init__(self, num_entities, num_relations, initial_tensor, gradient_mask=None):
         """
@@ -29,13 +30,14 @@ class RESCAL(torch.nn.Module):
 
     def forward(self, subject_index, relation_index):
         core_tensor = get_gradient_masked_tensor_clone(self.core_tensor, self.gradient_mask)  # (d_e, n_r, d_e)
-        relation_matrix = core_tensor[:, relation_index, :]                                   # (d_e, d_e)
-        subject = self.entity_embeddings(torch.tensor(subject_index))                         # (d_e)
+        relation_matrix = core_tensor[:, relation_index, :].permute(1, 0, 2)                  # (bs, d_e, d_e)
+        subject = self.entity_embeddings(torch.tensor(subject_index))                         # (bs, d_e)
         objects = self.entity_embeddings.weight                                               # (n_e, d_e)
 
-        output = torch.matmul(subject.unsqueeze(0), relation_matrix)          # (1, d_e) * (d_e, d_e) = (1, d_e)
-        output = torch.matmul(output, torch.transpose(objects, 0, 1))         # (1, d_e) * (d_e, n_e) = (1, n_e)
-        output = output.squeeze()                                             # (1, n_e) => (n_e)
+        output = torch.bmm(subject.unsqueeze(1), relation_matrix)       # (bs, 1, d_e) * (bs, d_e, d_e) = (bs, 1, d_e)
+        expanded_objects = torch.transpose(objects, 0, 1).expand(len(subject_index), -1, -1)
+        output = torch.matmul(output, expanded_objects)                 # (bs, 1, d_e) * (bs, d_e, n_e) = (bs, 1, n_e)
+        output = output.squeeze()                                       # (bs, 1, n_e) => (bs, n_e)
         sigmoid = torch.nn.Sigmoid()
         output = sigmoid(output)
         return output
@@ -43,4 +45,4 @@ class RESCAL(torch.nn.Module):
 
 if __name__ == '__main__':
     model = RESCAL(7, 8, np.random.normal(size=[3, 8, 3]).astype(np.float32))
-    output = model(0, 1)
+    output = model([0, 1], [0, 1])
