74de8351c8b4fc84cca6bdf7c2902deb0425e206
Lammatian <mateusz.parafinski@gmail.com>
2021-03-31 19:58:41 +0200

    Add minimal documentation

--- data_loader.py
+++ data_loader.py
@@ -22,6 +22,15 @@ class DataLoader:
         self._determine_relation_mapping()
 
     def get_1_to_n_train_data(self):
+        '''
+        Get the training data as two dictionaries:
+        - one with the pairs (s, r) as keys and a list of objects o as values
+          such that facts (s, r, o) are in the dataset
+        - the other with the pairs (r, o) as keys and a list of objects s as
+          values such that facts (s, r, o) are in the dataset
+        
+        This function returns the indices of the entities and relations
+        '''
         sr_pairs = defaultdict(list)
         ro_pairs = defaultdict(list)
 
@@ -35,6 +44,12 @@ class DataLoader:
         return sr_pairs, ro_pairs
 
     def map_data_to_indices(self, dataset: str):
+        '''
+        Given a dataset (one of train/valid/test), return all the facts (s,
+        r, o) present in the dataset.
+
+        This function returns the indices of the entities and relations
+        '''
         if dataset not in ['train', 'valid', 'test']:
             raise DataLoaderException('Wrong dataset name (required one of train/valid/test)')
 
@@ -49,11 +64,19 @@ class DataLoader:
         return mapped_data
 
     def get_embeddings(self, de: int, dr: int):
+        '''
+        Given an entity embedding dimension `de` and the relation embedding
+        dimension `dr`, return the embeddings for the entities and relations in
+        the dataset
+        '''
         E = torch.nn.Embedding(len(self.entities), de)
         R = torch.nn.Embedding(len(self.relations), dr)
         return E, R
 
     def _get_entities(self):
+        '''
+        Get all the entities present in any of the datasets
+        '''
         entities = set()
 
         for _, d in self.data.items():
@@ -62,6 +85,9 @@ class DataLoader:
         return entities
 
     def _get_relations(self):
+        '''
+        Get all the relations present in any of the datasets
+        '''
         relations = set()
 
         for _, d in self.data.items():
@@ -70,9 +96,15 @@ class DataLoader:
         return relations
 
     def _determine_entity_mapping(self):
+        '''
+        Map entities to indices from 1 to len(self.entities) - 1
+        '''
         self.entity_to_idx = {e: i for i, e in enumerate(self.entities)}
 
     def _determine_relation_mapping(self):
+        '''
+        Map relations to indices from 1 to len(self.relations) - 1
+        '''
         self.relation_to_idx = {r: i for i, r in enumerate(self.relations)}
 
 
