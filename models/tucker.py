877b278182238186fae6dea6adb561343e1d9b36
Sharan <sharan20051998@gmail.com>
2021-04-01 00:28:40 +0100

    Added initializing arguments for entity and relation embeddings in TuckER class

--- models/tucker.py
+++ models/tucker.py
@@ -1,9 +1,11 @@
 import torch
 import numpy as np
 
-# x: tensor of shape b x * x d x * where * denotes any dimensions in between
-# y: matrix of shape b x d
 def batched_tensorvectormul(x, y, axis):
+    '''
+    x: tensor of shape b x * x d x * where * denotes any dimensions in between
+    y: matrix of shape b x d
+    '''
     assert(len(y.shape) == 2)
     assert(axis in range(1, len(x.shape)))
     assert(x.shape[0] == y.shape[0])
@@ -14,20 +16,24 @@ def batched_tensorvectormul(x, y, axis):
     product = torch.sum(broadcasted_hadamard, axis=axis)
     return product
 
-# creates a copy of 'tensor' that sends non zero gradients only at areas marked as '1' in 'grad_mask'
 def get_gradient_masked_tensor_clone(tensor, grad_mask):
+    '''
+    Creates a copy of 'tensor' that sends non zero gradients only at areas marked as '1' in 'grad_mask'
+    '''
     tensor_clone = tensor.detach()
     tensor = tensor * grad_mask
     tensor_clone = tensor_clone * (1.0 - grad_mask)
     return tensor + tensor_clone
 
 
-# core of shape e x r x e
-# s of shape b x e
-# r of shape b x r
-# o of shape n x e
-# b refers to the batch size, and n the number of objects
 def tucker_multiplication(core, s, r, o):
+    '''
+    core of shape e x r x e
+    s of shape b x e
+    r of shape b x r
+    o of shape n x e
+    b refers to the batch size, and n the number of objects
+    '''
     assert(len(s.shape) == 2 and len(r.shape) == 2 and len(o.shape) == 2)
     assert(len(core.shape) == 3)
     assert(s.shape[0] == r.shape[0])
@@ -45,7 +51,15 @@ def tucker_multiplication(core, s, r, o):
 
 
 class TuckER(torch.nn.Module):
-    def __init__(self, num_entities, num_relations, initial_tensor, gradient_mask=None):
+    def __init__(
+            self, 
+            num_entities: int, 
+            num_relations: int, 
+            initial_tensor, 
+            gradient_mask=None, 
+            initial_entity_embeddings=None,
+            initial_relation_embeddings=None
+        ):
         if gradient_mask is None:
             gradient_mask = np.ones(initial_tensor.shape, dtype=np.float32)
 
@@ -60,6 +74,18 @@ class TuckER(torch.nn.Module):
 
         self.entity_embeddings = torch.nn.Embedding(num_entities, entity_embedding_dim)
         self.relation_embeddings = torch.nn.Embedding(num_relations, relation_embedding_dim)
+        if initial_entity_embeddings is not None:
+            self.set_entity_embeddings(initial_entity_embeddings)
+        if initial_relation_embeddings is not None:
+            self.set_relation_embeddings(initial_relation_embeddings)
+    
+    def set_entity_embeddings(self, entity_embeddings):
+        entity_embeddings = torch.from_numpy(entity_embeddings)
+        self.entity_embeddings.weight.data.copy_(entity_embeddings)
+
+    def set_relation_embeddings(self, relation_embeddings):
+        relation_embeddings = torch.from_numpy(relation_embeddings)
+        self.relation_embeddings.weight.data.copy_(relation_embeddings)
 
     def forward(self, subject_index, relation_index):
         core_tensor = get_gradient_masked_tensor_clone(self.core_tensor, self.gradient_mask)
