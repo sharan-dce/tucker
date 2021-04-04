fd4bf6e37266239ea06616ca4b9aa32a9e3d6815
Lammatian <mateusz.parafinski@gmail.com>
2021-04-04 22:49:05 +0200

    Add a function for 1-N data on the validation set

--- data_loader.py
+++ data_loader.py
@@ -57,6 +57,28 @@ class DataLoader:
         '''
         return self.sr_pairs, self.ro_pairs
 
+    def get_1_to_n_valid_data(self) -> Tuple[Dict[Tuple[int, int, Set[int]]], Dict[Tuple[int, int], Set[int]]]:
+        '''
+        Get the validation data as two dictionaries:
+        - one with the pairs (s, r) as keys and a set of objects o as values
+          such that facts (s, r, o) are in the dataset
+        - the other with the pairs (r, o) as keys and a set of objects s as
+          values such that facts (s, r, o) are in the dataset
+
+        This function returns the indices of the entities and relations
+        '''
+        sr_pairs = defaultdict(set)
+        ro_pairs = defaultdict(set)
+
+        for s, r, o in self.data['valid']:
+            s_idx = self.entity_to_idx[s]
+            r_idx = self.relation_to_idx[r]
+            o_idx = self.entity_to_idx[o]
+            sr_pairs[(s_idx, r_idx)].add(o_idx)
+            ro_pairs[(r_idx, o_idx)].add(s_idx)
+
+        return sr_pairs, ro_pairs
+
     def _determine_1_to_n_train_data(self) -> Tuple[Dict[Tuple[int, int], Set[int]], Dict[Tuple[int, int], Set[int]]]:
         '''
         Reorganise the training data such that for all pairs (s, r) of a
