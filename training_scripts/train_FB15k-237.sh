345f92b08df73af8f6db0219438742df7c899aac
Lammatian <mateusz.parafinski@gmail.com>
2021-04-08 10:51:08 +0200

    Update training scripts
    Make them work on Sharan's devices + add timestamping.

--- training_scripts/train_FB15k-237.sh
+++ training_scripts/train_FB15k-237.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --job-name=sneaky
+#SBATCH --gres=gpu:p100:1
+#SBATCH --account=engs-tvg
+ 
+module load gpu/cuda/10.1.243
+module load gpu/cudnn/7.6.5__cuda-10.1
+
+source activate sneaky_env
+
+DATE=$(date +"%H.%M.%S_%d-%m-%Y")
+FILENAME="FB15k-237_$DATE.txt"
+
 python3 -u train_script.py\
     --model=tucker\
     --datapath=../data/FB15k-237\
@@ -11,4 +26,4 @@ python3 -u train_script.py\
     --input_dropout=0.3\
     --hidden_dropout1=0.4\
     --hidden_dropout2=0.5\
-    --label_smoothing=0.1 > FB15k-237_training_result.txt
+    --label_smoothing=0.1 > $FILENAME
--- training_scripts/train_FB15k.sh
+++ training_scripts/train_FB15k.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --job-name=sneaky
+#SBATCH --gres=gpu:p100:1
+#SBATCH --account=engs-tvg
+ 
+module load gpu/cuda/10.1.243
+module load gpu/cudnn/7.6.5__cuda-10.1
+
+source activate sneaky_env
+
+DATE=$(date +"%H.%M.%S_%d-%m-%Y")
+FILENAME="FB15k_$DATE.txt"
+
 python3 -u train_script.py\
     --model=tucker\
     --datapath=../data/FB15k\
@@ -11,4 +26,4 @@ python3 -u train_script.py\
     --input_dropout=0.2\
     --hidden_dropout1=0.2\
     --hidden_dropout2=0.3\
-    --label_smoothing=0.0 > FB15k_training_result.txt
+    --label_smoothing=0.0 > $FILENAME
