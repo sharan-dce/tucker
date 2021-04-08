28cac9793dab0f4df3c0d320fad83b8ff94c10ec
sharan.dce <sharan20051998@gmail.com>
2021-04-08 16:50:33 +0100

    FB15k-237 training done

---
+++ training_scripts/train_FB15k_rescal.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --job-name=sneaky
+#SBATCH --gres=gpu:p100:1
+#SBATCH --account=engs-tvg
+ 
+module load gpu/cuda/10.1.243
+module load gpu/cudnn/7.6.5__cuda-10.1
+
+source activate sneaky_env
+
+DATE=$(date +"%H.%M.%S_%d-%m-%Y")
+FILENAME="FB15k_$DATE_rescal.txt"
+
+python3 -u train_script.py\
+    --model=rescal\
+    --datapath=../data/FB15k\
+    --num_iterations=500\
+    --batch_size=64\
+    --lr=0.003\
+    --dr=0.99\
+    --edim=200\
+    --rdim=200\
+    --cuda=True\
+    --input_dropout=0.2\
+    --hidden_dropout1=0.2\
+    --hidden_dropout2=0.3\
+    --label_smoothing=0.0 > $FILENAME
